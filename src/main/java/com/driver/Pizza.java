6b4278aa2146a16532a596cc8745694e48e9015a
shrutiranawade <ranawadeshruti@gmail.com>
2023-02-19 01:50:45 +0530

    pizza assignment

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -3,12 +3,27 @@ package com.driver;
 public class Pizza {
 
     private int price;
+    private int toppings;
     private Boolean isVeg;
     private String bill;
+private boolean isExtraToppingAdded;
+private boolean isExtraCheeseAdded;
+private boolean isTakeAway;
+private boolean isBillCreated;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if(isVeg){
+            this.price =300;
+            this.toppings = 70;
+
+        }
+        else{
+        this.price = 400;
+        this.toppings = 80;
+    }
+        this.bill = "Base Price Of The Pizza: "+this.price;
     }
 
     public int getPrice(){
@@ -17,18 +32,43 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if(!isExtraCheeseAdded){
+            this.price += 80;
+            isExtraCheeseAdded = true;
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(!isExtraToppingAdded){
+            this.price += this.toppings;
+            isExtraToppingAdded = true;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(!isTakeAway){
+            this.price += 20;
+            isTakeAway =true;
+        }
     }
 
     public String getBill(){
         // your code goes here
+        if (isBillCreated) {
+            if (isExtraCheeseAdded) {
+                this.bill = "Extra Cheese Added: 80" + "\n";}
+                if (isExtraToppingAdded) {
+                    this.bill = "Extra Toppings Added: " + this.toppings + "\n";
+                }
+                if (isTakeAway) {
+                    this.bill = "Paperbag Added: 20";
+                }
+                this.bill = "Total Price: " + this.bill + "\n";
+                isBillCreated =true;
+
+        }
         return this.bill;
     }
 }
